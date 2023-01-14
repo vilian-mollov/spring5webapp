a1de99e9f241c9f48f17a7fcdf0ef1f101da968e
Avalon <vilian3737@gmail.com>
2023-01-14 12:30:36 +0200

    Modified PublisherRepository.java and added some code in BootStrapData.java for validation

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -14,10 +16,12 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository,PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -52,6 +56,16 @@ public class BootStrapData implements CommandLineRunner {
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Books: " + bookRepository.count());
+
+
+
+        Publisher adison = new Publisher("Addison-Wesley Professional",
+                "Wall Street 221","New York","Northeastern","Z47-18");
+
+        System.out.println(adison);
+        publisherRepository.save(adison);
+        System.out.println(adison);
+        System.out.println("Publishers in repository: " + publisherRepository.count());
 
 
     }
--- src/main/java/guru/springframework/spring5webapp/repositories/PublisherRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/PublisherRepository.java
@@ -3,6 +3,7 @@ package guru.springframework.spring5webapp.repositories;
 
 import guru.springframework.spring5webapp.domain.Publisher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface PublisherRepository extends JpaRepository<Publisher,Long> {
+public interface PublisherRepository extends CrudRepository<Publisher,Long> {
 }
