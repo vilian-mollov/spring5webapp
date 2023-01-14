4c52511b771fc9482b0822ca0009f3118453ee9a
Avalon <vilian3737@gmail.com>
2023-01-14 12:31:29 +0200

    Modified PublisherRepository.java

--- src/main/java/guru/springframework/spring5webapp/repositories/PublisherRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/PublisherRepository.java
@@ -2,7 +2,6 @@ package guru.springframework.spring5webapp.repositories;
 
 
 import guru.springframework.spring5webapp.domain.Publisher;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PublisherRepository extends CrudRepository<Publisher,Long> {
